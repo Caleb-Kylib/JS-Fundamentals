c1f7a1de32a63631eaf68ded0df189d9d2c7c7c6
caleb-kylib <lemayiancaleb18@gmail.com>
2025-07-13 20:27:30 +0300

    Fix: pluralize 0 arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,7 @@
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
-  console.log("0 argument");
+  console.log("0 arguments");
 } else if (args.length === 1) {
   console.log("1 argument");
 } else {
